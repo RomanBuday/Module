d7ae9af4b981ba743a3cb6ee8f031f99906cb782
Roman Budai <roman1985buday@gmail.com>
2022-03-17 11:31:36 +0200

    final commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+//     $('.slider').slick();
+// });
+
+$('.single-slide').slick({
+    
+});
+
+$('.one-time').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    centerMode: true,
+    arrows: false,
+    variableWidth: true,
+    // prevArrow: '<span class="icon-angle-left left-arrw"></span>', 
+    // nextArrow: '<span class="icon-angle-right right-arrw"></span>', 
+  });
